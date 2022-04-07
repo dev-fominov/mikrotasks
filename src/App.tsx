bf6a2d608fdf303bcce5027dcf2a2bb2aeb7edf5
dev-fominov <dev-fominov@yandex.by>
2022-04-07 19:01:07 +0300

    task button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Car from './Car';
+import Car from './tsk_1/Car';
+import Button from './tsk_2/Button';
 
 const topCars = [
   { manufacturer: 'BMW', model: 'm5cs' },
@@ -9,9 +10,22 @@ const topCars = [
 ]
 
 function App() {
+
+  const Btn1Foo = (subscriber: string) => {
+    console.log(subscriber)
+  }
+
+  const Btn2Foo = (subscriber: string) => {
+    console.log(subscriber)
+  }
+  
+
   return (
     <div>
       <Car topCars={topCars} />
+      <br />
+      <Button name={'Name 1'} callBack={() => Btn1Foo('Vasya')} />
+      <Button name={'Name 2'} callBack={() => Btn2Foo('Ivan')} />
     </div>
   );
 }
